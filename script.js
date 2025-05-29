5ee4855d68f810d435fd8339f52dce2e514c9a3f
Shailesh Pathak <shaileshpathak1674@gmail.com>
2025-05-29 09:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,32 @@
-        var errormessage = "";
-        var missingfields = "";
-        
-        // Function to check if the email is valid
-        function isEmail(email) {
-            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            return regex.test(email);
-        }   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROJECT-Form Validation</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="errors"></div>
 
-        $("#submit").click(function(){
-            
-            if($("#Email").val() == ""){
-                missingfields += "<p> Email is required. </p>";
-                
-            }
-            if($("#phoneno").val() == ""){
-                missingfields += "<p> Phone number is required. </p>";
-                
-            }
-            if($("#pass").val() == ""){
-                missingfields += "<p> Password is required. </p>";
-                
-            }
-            if($("#confirmpass").val() == ""){
-                missingfields += "<p> Confirm Password is required. </p>";
-               
-            }
+    <div class="container">
+        <div class="forms">
+            <label for="Email">Email: </label>
+            <input type="email" name="Email" id="Email">
+            <label for="PhoneNo">Phone Number: </label>
+            <input type="text" name="PhoneNo" id="phoneno">
+            <label for="pass">Password: </label>
+            <input type="password" name="pass" id="pass">
+            <label for="confirmpass">Confirm Password: </label>
+            <input type="password" name="confirmpass" id="confirmpass">
+        </div>
+        <input type="submit" value="Submit" id="submit">
+    </div>
+    <div id="correct"></div>
 
-            // checking if email is correct or not
-            if(isEmail($("#Email").val()) == false){
-                errormessage += "<p> Please enter a valid email address.</p>";
-                
-            }
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="script.js"></script>
 
-            //confirming that phone number is 10 digits, not empty, and is a number
-            if($("#phoneno").val()){
-                if($.isNumeric($("#phoneno").val()) == false ) {
-                    errormessage += "<p> Phone number should be a number. </p>";
-                   
-                }
-            } 
 
-            //checking weather password and confirm password is sameor not
-            if($("#pass").val() != $("#confirmpass").val()){
-                errormessage += "<p> Password and Confirm Password should be same. </p>";
-            
-            }
-            if(errormessage == "" && missingfields == ""){
-                $("#correct").html("You are registered successfully!").fadeIn();
-                
-            }
-            else{
-                $("#errors").html(missingfields + errormessage).fadeIn();
-            }
-        })
+</body>
+</html>
